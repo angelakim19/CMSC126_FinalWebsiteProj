f9a4089e0e04074c9f2974dacfe66d0b109fff11
Quennie Nebria <quenn.nebria@gmail.com>
2024-05-26 10:37:48 +0800

    index css then nag add sd ko raw css for front.

---
+++ redirect.js
@@ -0,0 +1,7 @@
+function redirectToRegister() {
+    window.location.href = 'register.html'; // Replace with the actual path to your register page
+}
+
+function redirectToLogin() {
+    window.location.href = 'login.html'; // Replace with the actual path to your login page
+}
